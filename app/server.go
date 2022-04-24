f97d466d3f93aecdf430b64583c0b3fc11a87d39
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-04-25 02:18:06 +0900

    feat: delete, route json

--- app/server.go
+++ app/server.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"myapp/database"
 )
@@ -18,6 +21,21 @@ type User struct {
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+	// 	// SigningKey: []byte(os.Getenv("JWT_SECRET")),
+	// 	SigningKey: []byte("secret"),
+	// }))
+
+	e.Pre(middleware.RemoveTrailingSlash())
 	
 	// --------------------------------------------------------------------------------
 	e.GET("/", func(c echo.Context) error {
@@ -28,9 +46,19 @@ func main() {
 		return c.String(http.StatusOK, "Hello, world")
 	})
 
+	
+
 	e.GET("/users", getUsers)
 	e.GET("/users/:id", getUserById)
+	e.DELETE("/users/:id", deleteUser)
 	// --------------------------------------------------------------------------------
+
+	// export routes data to json file
+	data, err := json.MarshalIndent(e.Routes(), "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	ioutil.WriteFile("routes.json", data, 0644)
 	
 	// set port
 	port := os.Getenv("PORT")
@@ -72,6 +100,27 @@ func getUserById(c echo.Context) error {
 	}
 
 	result := db.First(&user, id)
+
+	if result.Error != nil {
+		return c.String(http.StatusInternalServerError, result.Error.Error())
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
+
+func deleteUser(c echo.Context) error {
+	
+	db := database.Connect()
+
+	var user User
+
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	result := db.Delete(&user, id)
 
 	if result.Error != nil {
 		return c.String(http.StatusInternalServerError, result.Error.Error())
